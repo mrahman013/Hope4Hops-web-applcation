fd03fb6fe57c7ac2a7d82c37801f4102cbbcf118
Kaitlyn Chait <katiec1029@gmail.com>
2017-11-09 15:09:59 -0500

    trying to fix the deployment

--- hopsapp/__init__.py
+++ hopsapp/__init__.py
@@ -1,7 +1,6 @@
 """Implements a basic flask app that provides hashes of text."""
 from flask import Flask
 app = Flask(__name__)
+import hopsapp.routes
 
 app.config['DEBUG'] = True
-
-import hopsapp.routes
