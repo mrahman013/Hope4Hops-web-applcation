5aafc1951d84d8ab605c1c6518d505d576fff9a6
Kaitlyn Chait <katiec1029@gmail.com>
2017-11-09 16:17:19 -0500

    debugged app and updated req txt

--- hopsapp/__init__.py
+++ hopsapp/__init__.py
@@ -1,9 +1,10 @@
 """Implements a basic flask app that provides hashes of text."""
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import hopsapp.routes
 
 app = Flask(__name__)
+
+from flask_sqlalchemy import SQLAlchemy
+import hopsapp.routes
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yjjuylsytqewni:d0d63322c6abd33e2dadeafd7ef2501f73af54cf2d39596e464ea2c18b0234a3@ec2-23-23-78-213.compute-1.amazonaws.com:5432/d3gdnt7fkmonn1'
 
