1df23f8f9a79db7ed94775c5e185fe6cbe4eeb59
Kaitlyn Chait <katiec1029@gmail.com>
2017-11-09 16:34:04 -0500

    added SQLALCHEMY_TRACK_MODIFICATIONS

--- hopsapp/__init__.py
+++ hopsapp/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 import hopsapp.routes
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yjjuylsytqewni:d0d63322c6abd33e2dadeafd7ef2501f73af54cf2d39596e464ea2c18b0234a3@ec2-23-23-78-213.compute-1.amazonaws.com:5432/d3gdnt7fkmonn1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app) #TODO: assure this works
 # print("db: ", db) #NOTE: FOR TESTING PURPOSES, NEEDS TO BE DELETED WHEN TEST FILES CREATES
