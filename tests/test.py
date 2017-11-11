347fd7023c246623af2ce7a180495dae12c7c896
max <maxraphael_inciong@yahoo.com>
2017-11-10 20:29:21 -0500

    made a template for test.py

--- tests/test.py
+++ tests/test.py
@@ -5,34 +5,11 @@ import hopsapp.models
 import tempfile
 import unittest
 
-@pytest.fixture
-def app(request):
+class hoptest(unittest.TestCase):
+	def test_home:
 
-    db_fd, temp_db_location = tempfile.mkstemp()
-    config = {
-        'DATABASE': temp_db_location,
-        'TESTING': True,
-        'DB_FD': db_fd
-    }
-
-    app = create_app(config=config)
-
-    with app.app_context():
-        init_db()
-        yield app
-
-
-@pytest.fixture
-def client(request, app):
-
-    client = app.test_client()
-
-    def teardown():
-        os.close(app.config['DB_FD'])
-        os.unlink(app.config['DATABASE'])
-    request.addfinalizer(teardown)
-
-    return client
-
+if __name__=='__main__':
+	unittest.main
+	
 
 
