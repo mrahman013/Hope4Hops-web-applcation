da70730cd577e55655e227b54c797e9e5e1b80e8
Chris Panican <chrispanican@gmail.com>
2017-11-17 21:33:03 -0500

    Add about page and routes

--- hopsapp/routes.py
+++ hopsapp/routes.py
@@ -10,9 +10,13 @@ from hopsapp import app
 def home():
     return render_template("home.html")
 
-# @app.route('/about')
-# def about():
-#     return render_template("")  #insert name of template here
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
