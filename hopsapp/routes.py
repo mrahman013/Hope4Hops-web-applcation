84a2c51d8883cf2272e0f52f2e064740dc6ebcd9
Md Rahman <mrahman013@citymail.cuny.edu>
2017-11-26 14:15:07 -0500

    distance rounded off in 2 decimal places

--- hopsapp/routes.py
+++ hopsapp/routes.py
@@ -108,26 +108,13 @@ def findstore():
         r = distance(user_lat, user_lon, store_lat[i], store_lon[i])
         distance_from_user.append(r)
     
-    print(store_name)
-    print(store_address)
-    print(store_city)
-    print(store_state)
-    print(store_zip)
-    print(store_avg_traffic)
-    print(store_lat)
-    print(store_lon)
+   
     print(distance_from_user)
     # sorting all according to distance
     distance_from_user, store_name, store_address, store_city, store_state, store_zip, store_avg_traffic, store_lat, store_lon = zip(*sorted(zip(distance_from_user, store_name, store_address, store_city, store_state, store_zip, store_avg_traffic, store_lat, store_lon)))   
+    distance_from_user = [ '%.2f' % elem for elem in distance_from_user ]
     print("----------------------------------------------")
-    print(store_name)
-    print(store_address)
-    print(store_city)
-    print(store_state)
-    print(store_zip)
-    print(store_avg_traffic)
-    print(store_lat)
-    print(store_lon)
+    
     print(distance_from_user)
 
     # tem2D = [{"name": "store A", "zip": 11219},
