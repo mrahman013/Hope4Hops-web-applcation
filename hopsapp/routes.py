900c28e98e1a5d4a06fd4853fdbc90a759c749c9
Md Rahman <mrahman013@citymail.cuny.edu>
2017-11-28 00:48:10 -0500

    changing beer profile form routes to take all component of stores

--- hopsapp/routes.py
+++ hopsapp/routes.py
@@ -24,6 +24,26 @@ def find_rare_beers():
 def staff_beers():
     return Beer.query.limit(3)
 
+# def distance_from_user(beer):
+#     def distance(lat1, lon1, lat2, lon2):
+#         conv_fac = 0.621371 # conversion factor
+#         p = 0.017453292519943295     #Pi/180
+#         a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+#         kil_m = 12742 * asin(sqrt(a)) #2*R*asin...
+#         miles = kil_m * conv_fac
+#         miles = float("{0:.1f}".format(miles))
+#         return miles
+#     #TODO: get user latitude and longitude instead of using hardcoded
+#     user_lat = 40.8200471
+#     user_lon = -73.9514611
+#     distances = []
+#     for store in beer.stores:
+#         d = distance(user_lat, user_lon, store.lat, store.lon)
+#         distances.append(d)
+#     return distances
+
+
+
 def distance_from_user(beer):
     def distance(lat1, lon1, lat2, lon2):
         conv_fac = 0.621371 # conversion factor
@@ -36,11 +56,35 @@ def distance_from_user(beer):
     #TODO: get user latitude and longitude instead of using hardcoded
     user_lat = 40.8200471
     user_lon = -73.9514611
-    distances = []
+    
+    store_name = []
+    store_address = []
+    store_city = []
+    store_state = []
+    store_zip = []
+    store_avg_traffic = []
+    store_lat = []
+    store_lon = []
+    distance_from_user = []
+
+
     for store in beer.stores:
         d = distance(user_lat, user_lon, store.lat, store.lon)
-        distances.append(d)
-    return distances
+        distance_from_user.append(d)
+        store_name.append(store.name)
+        store_address.append(store.address)
+        store_city.append(store.city)
+        store_state.append(store.state)
+        store_zip.append(store.zip_code)
+        store_avg_traffic.append(store.average_traffic)
+        store_lat.append(store.lat)
+        store_lon.append(store.lon)
+    # sorting all according to distance
+    distance_from_user, store_name, store_address, store_city, store_state, store_zip, store_avg_traffic, store_lat, store_lon = zip(*sorted(zip(distance_from_user, store_name, store_address, store_city, store_state, store_zip, store_avg_traffic, store_lat, store_lon)))   
+    all_component = zip(store_name, store_address, store_city, store_state, store_zip, store_avg_traffic, store_lat, store_lon, distance_from_user)
+    return all_component
+
+
 
 
 @app.route('/', methods=['GET','POST'])
@@ -114,8 +158,9 @@ def beerprofile():
     if request.method == 'GET':
         search = request.args['name']
         beer = Beer.query.filter_by(name=search).first()
-        distances = distance_from_user(beer)
-        return render_template("beerprofile.html",beer=beer,distances=distances)
+        store_component = distance_from_user(beer)
+        # change made hare
+        return render_template("beerprofile.html",beer=beer,all_component=store_component)
         
 
     elif request.method == 'POST':
