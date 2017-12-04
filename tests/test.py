37da9950a837654882059305bd9f5c9e8fe1477f
Md Rahman <mrahman013@citymail.cuny.edu>
2017-12-04 16:55:12 -0500

    few test done

--- tests/test.py
+++ tests/test.py
@@ -6,34 +6,38 @@ from hopsapp.routes import distance
 class hoptest(unittest.TestCase):
 	def setUp(self):
 		self.app = hopsapp.app.test_client()
-	# def test_home(self):
-	# 	response = self.app.get('/')
-	# 	self.assertEqual(response.status_code, 200)
-	# 	self.assertIn('home', response.data)
-	# def test_about(self):
-	# 	response = self.app.get('/about')
-	# 	self.assertEqual(response.status_code, 200)
-	# 	self.assertIn('about', response.data)
-	# def test_login(self):
-	# 	response = self.app.get('/login')
-	# 	self.assertEqual(response.status_code, 200)
-	# 	self.assertIn('login', response.data)
-	# def test_register(self):
-	# 	response = self.app.get('/register')
-	# 	self.assertEqual(response.status_code, 200)
-	# 	self.assertIn('register', response.data)
-	# def test_beerprofile(self):
-	# 	response = self.app.get('/beerprofile')
-	# 	self.assertEqual(response.status_code, 200)
-	# 	self.assertIn('beerprofile', response.data)
-	# def test_breweryprofile(self):
-	# 	response = self.app.get('/breweryprofile')
-	# 	self.assertEqual(response.status_code, 200)
-	# 	self.assertIn('breweryprofile', response.data)
-	# def test_storeprofile(self):
-	# 	response = self.app.get('/storeprofile')
-	# 	self.assertEqual(response.status_code, 200)
-	# 	self.assertIn('storeprofile', response.data)
+	def test_home(self):
+		response = self.app.get('/')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('home', response.data)
+	def test_about(self):
+		response = self.app.get('/about')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('about', response.data)
+	def test_login(self):
+		response = self.app.get('/login')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('login', response.data)
+	def test_register(self):
+		response = self.app.get('/register')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('register', response.data)
+	def test_beerprofile(self):
+		response = self.app.get('/beerprofile')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('beerprofile', response.data)
+	def test_breweryprofile(self):
+		response = self.app.get('/breweryprofile')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('breweryprofile', response.data)
+	def test_storeprofile(self):
+		response = self.app.get('/storeprofile')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('storeprofile', response.data)
+
+
+
+# All test below are passed
 
 	def test_contact(self):
 		response = self.app.get('/contact')
@@ -59,7 +63,8 @@ class hoptest(unittest.TestCase):
 		response = self.app.get('/')
 		self.assertIn(b'6.00%', response.data)			
 		self.assertIn(b'0.0', response.data)			
-		self.assertIn(b'COMMON', response.data)			
+		self.assertIn(b'COMMON', response.data)		
+
 	# Testing distance function
 	def test_distance_func(self):
 		self.assertEqual(6.6, distance(40.8200471, -73.9514611, 40.727588,-73.983858))
