d5aac9c7ee6e25f25bc837e8837e2ba10da81a7b
Md Rahman <mrahman013@citymail.cuny.edu>
2017-12-04 16:24:54 -0500

    database uri and trakmodification unittest

--- tests/test.py
+++ tests/test.py
@@ -4,8 +4,8 @@ import unittest
 from hopsapp.routes import distance
 
 class hoptest(unittest.TestCase):
-	# def setUp(self):
-	# 	self.app = hopsapp.app.test_client()
+	def setUp(self):
+		self.app = hopsapp.app.test_client()
 	# def test_home(self):
 	# 	response = self.app.get('/')
 	# 	self.assertEqual(response.status_code, 200)
@@ -41,6 +41,12 @@ class hoptest(unittest.TestCase):
 
 	def test_config_debug(self):
 		assert app.config['DEBUG'] is True
+
+	def test_config_database_URI(self):
+		assert app.config['SQLALCHEMY_DATABASE_URI'] == 'postgres://yjjuylsytqewni:d0d63322c6abd33e2dadeafd7ef2501f73af54cf2d39596e464ea2c18b0234a3@ec2-23-23-78-213.compute-1.amazonaws.com:5432/d3gdnt7fkmonn1'
+
+	def test_config_trackmodification(self):
+		assert app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] is True
 
 	# did not work
 	# def test_beerprofile_intigration(self):
