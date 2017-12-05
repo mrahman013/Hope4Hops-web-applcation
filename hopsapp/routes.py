477ea3608183e07d184d02d4aa8be7f4652379e4
Andre <awong21@citymail.cuny.edu>
2017-12-05 15:24:20 -0500

    Created 405 Error Handler

--- hopsapp/routes.py
+++ hopsapp/routes.py
@@ -242,6 +242,11 @@ def storeprofile():
 def not_found_error(error):
     return render_template('404.html'), 404
 
+@app.errorhandler(405)
+def not_found_error(error):
+    return render_template('405.html'), 405
+
+
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
