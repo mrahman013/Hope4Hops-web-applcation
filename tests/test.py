898cb4d8c9aa1a733dc4e792cac0919de51d0fc3
Andre <awong21@citymail.cuny.edu>
2017-12-11 18:30:44 -0500

    Fixed a few awkward issues from previous commit

--- tests/test.py
+++ tests/test.py
@@ -41,19 +41,19 @@ class hoptest(unittest.TestCase):
 	def test_error_404(self):
 		response = self.app.get('/404')
 		self.assertEqual(response.status_code, 200)
-		self.assertIn('contact', response.data)
+		self.assertIn('404', response.data)
 	def test_error_400(self):
 		response = self.app.get('/400')
 		self.assertEqual(response.status_code, 200)
-		self.assertIn('contact', response.data)
+		self.assertIn('400', response.data)
 	def test_error_405(self):
 		response = self.app.get('/405')
 		self.assertEqual(response.status_code, 200)
-		self.assertIn('contact', response.data)
+		self.assertIn('405', response.data)
 	def test_error_500(self):
 		response = self.app.get('/500')
 		self.assertEqual(response.status_code, 200)
-		self.assertIn('contact', response.data)
+		self.assertIn('500', response.data)
 
 
 
@@ -73,14 +73,14 @@ class hoptest(unittest.TestCase):
 	# testing if home is getting beer style from database and showing on page when page load
 	def test_home_beer_style(self):
 		response = self.app.get('/')
-		self.assertIn(b'ALE', response.data)	
-	
+		self.assertIn(b'ALE', response.data)
+
 	# testing if home is getting beer ABV, rating, rarity from database and showing on page when page load
 	def test_home_beer_others_content(self):
 		response = self.app.get('/')
-		self.assertIn(b'6.00%', response.data)			
-		self.assertIn(b'0.0', response.data)			
-		self.assertIn(b'COMMON', response.data)		
+		self.assertIn(b'6.00%', response.data)
+		self.assertIn(b'0.0', response.data)
+		self.assertIn(b'COMMON', response.data)
 
 	# Testing distance function
 	def test_distance_func(self):
@@ -101,39 +101,57 @@ class hoptest(unittest.TestCase):
 	# 	#self.assertIn(b'Boat', response.data)
 	# 	self.assertIn(b'Boat', response.data)
 
-	#have not tested yet
+	#Helper functions
+	def register_check(self, email, password, confirm):
+		return self.app.post('/register', data=dict(email=email, password=password, confirm=confirm), follow_redirects=True)
+
+	def login_check(self, email, password):
+		return self.app.post('/login', data=dict(email=email, password=password), follow_redirects=True)
+
+	def logout_check(self):
+		return self.app.get('/logout', follow_redirects=True)
+
+	#Testing registers
 	def test_user_register(self):
-		with self.client:
-		response=self.get('/register')
-		self.assertIn(b'johndoe@gmail.com', response.data)
-		self.assertIn(b'Password123', response.data)
-		self.assertIn(b'Password123', response.data)
+		response=self.register('johndoe123@gmail.com','Password123','Password123')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn(b'Cheers!', response.data)
 
 	def test_bad_user_register(self):
-		response=self.get('/register')
-		self.assertIn(b'Ooops! We apologize! There was an #error in your attempt to register.', response.data)
-	
-	def test_login(self):
-		response=self.post('/login',{email:'johndoe@gmail.com',passw#ord:'Password123',confirm:'Password123'})		self.assertEquals(response, 'johndoe@gmail.com')
+		response=self.register('badexample','Password123','notthesame')
+		self.assertIn(b'Ooops! We apologize! There was an error in your attempt to register.', response.data)
 
+	#Not tested
+	def test_bad_register_dupe(self):
+		response=self.register('johndoe123@gmail.com','Password123','Password123')
+		self.assertEqual(response.status_code,200)
+		response=self.register('johndoe123@gmail.com','Pass1word123','Pass1word123')
+		self.assertIn(b'Ooops! We apologize! There was an error in your attempt to register.', response.data))
+
+	def test_login(self):
+		response=self.login('johndoe@gmail.com','Password123')
+		self.assertEquals(response.status_code, 200)
+		self.assertIn(b'Logged in successfully',response.data)
+
+	def test_bad_login(self):
+		response=self.login('johndoe@gmail.com','passWord321')
+		self.assertIn(b'Failed Login Attempt',response.data)
 
 	def test_find_popular_beer_query(self):
 		expected=['07XX','Boat','Circus Boy']
-		result=self.Beer.query.order_by(desc(Beer.average_popularity#)).limit(3)
-
+		result=self.Beer.query.order_by(desc(Beer.average_popularity)).limit(3)
 		self.assertEqual(result, expected)
 
+	#Not tested
 	def test_find_rare_beer_query(self):
-		expected=['SNEAKBOX','SIM-NOTIC IMPERIAL IPA','Art #Hope Ale']
+		expected=['SNEAKBOX','SIM-NOTIC IMPERIAL IPA','Art Hope Ale']
 		result=self.Beer.query.order_by(Beer.rarity).limit(3)
 		self.assertEqual(result,expected)
-	
+
 	def test_staff_beer_query(self):
-		expected=['Heady Topper', 'SNEAKBOX', 'Feast of #Fools']
+		expected=['Heady Topper', 'SNEAKBOX', 'Feast of Fools']
 		result=self.Beer.query.limit(3)
 		self.assertEqual(result, expected)
 
 if __name__=='__main__':
 	unittest.main()
-	
-
