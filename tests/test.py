ad51759776c46bfe8e1a2912014c3071c0de61bb
Md Rahman <mrahman013@citymail.cuny.edu>
2017-12-12 00:53:28 -0500

    few units and interagation tests has fixed

--- tests/test.py
+++ tests/test.py
@@ -12,37 +12,37 @@ class TestUnitHopsapp(unittest.TestCase):
 		self.app = hopsapp.app.test_client()
 		self.response = self.app.get("/")
 
-# 	def test_home(self):
-# 		response = self.app.get('/')
-# 		self.assertEqual(response.status_code, 200)
-# 		#self.assertIn('home', response.data)
-# 	def test_about(self):
-# 		response = self.app.get('/about')
-# 		self.assertEqual(response.status_code, 200)
-# 		#self.assertIn('about', response.data)
-# 	def test_login(self):
-# 		response = self.app.get('/login')
-# 		self.assertEqual(response.status_code, 200)
-# 		#self.assertIn('login', response.data)
-# 	def test_register(self):
-# 		response = self.app.get('/register')
-# 		self.assertEqual(response.status_code, 200)
-# 		#self.assertIn('register', response.data)
-# 	def test_beerprofile(self):
-# 		response = self.app.get('/beerprofile?name=Boat')
-# 		self.assertEqual(response.status_code, 200)
-# # 		self.assertIn('beerprofile', response.data)
-# 	def test_breweryprofile(self):
-# 		response = self.app.get('/breweryprofile?name=Carton Brewing Company')
-# 		self.assertEqual(response.status_code, 200)
-# # 		self.assertIn('breweryprofile', response.data)
-# 	def test_storeprofile(self):
-# 		response = self.app.get('/storeprofile?name=Good Beer')
-# 		self.assertEqual(response.status_code, 200)
-# # 		self.assertIn('storeprofile', response.data)
-# 	def test_contact(self):
-# 		response = self.app.get('/contact')
-# 		self.assertEqual(response.status_code, 200)
+	def test_home(self):
+		response = self.app.get('/')
+		self.assertEqual(response.status_code, 200)
+		#self.assertIn('home', response.data)
+	def test_about(self):
+		response = self.app.get('/about')
+		self.assertEqual(response.status_code, 200)
+		#self.assertIn('about', response.data)
+	def test_login(self):
+		response = self.app.get('/login')
+		self.assertEqual(response.status_code, 200)
+		#self.assertIn('login', response.data)
+	def test_register(self):
+		response = self.app.get('/register')
+		self.assertEqual(response.status_code, 200)
+		#self.assertIn('register', response.data)
+	def test_beerprofile(self):
+		response = self.app.get('/beerprofile?name=Boat')
+		self.assertEqual(response.status_code, 200)
+# 		self.assertIn('beerprofile', response.data)
+	def test_breweryprofile(self):
+		response = self.app.get('/breweryprofile?name=Carton Brewing Company')
+		self.assertEqual(response.status_code, 200)
+# 		self.assertIn('breweryprofile', response.data)
+	def test_storeprofile(self):
+		response = self.app.get('/storeprofile?name=Good Beer')
+		self.assertEqual(response.status_code, 200)
+# 		self.assertIn('storeprofile', response.data)
+	def test_contact(self):
+		response = self.app.get('/contact')
+		self.assertEqual(response.status_code, 200)
 
 # 	def test_error_404(self):
 # 		response = self.app.get('/404')
@@ -62,56 +62,56 @@ class TestUnitHopsapp(unittest.TestCase):
 # 		self.assertIn('500', response.data)
 
 
-	# # Testing distance function
-	# def test_distance_func(self):
-	# 	self.assertEqual(6.6, distance(40.8200471, -73.9514611, 40.727588,-73.983858))
+	# Testing distance function
+	def test_distance_func(self):
+		self.assertEqual(6.6, distance(40.8200471, -73.9514611, 40.727588,-73.983858))
 
-	# def test_config_debug(self):
-	# 	assert app.config['DEBUG'] is True
+	def test_config_debug(self):
+		assert app.config['DEBUG'] is True
 
-	# def test_config_database_URI(self):
-	# 	assert app.config['SQLALCHEMY_DATABASE_URI'] == 'postgres://yjjuylsytqewni:d0d63322c6abd33e2dadeafd7ef2501f73af54cf2d39596e464ea2c18b0234a3@ec2-23-23-78-213.compute-1.amazonaws.com:5432/d3gdnt7fkmonn1'
+	def test_config_database_URI(self):
+		assert app.config['SQLALCHEMY_DATABASE_URI'] == 'postgres://yjjuylsytqewni:d0d63322c6abd33e2dadeafd7ef2501f73af54cf2d39596e464ea2c18b0234a3@ec2-23-23-78-213.compute-1.amazonaws.com:5432/d3gdnt7fkmonn1'
 
-	# def test_config_trackmodification(self):
-	# 	assert app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] is True
+	def test_config_trackmodification(self):
+		assert app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] is True
 
 
-# # All test below are passed
-# class TestIntegration(unittest.TestCase):
-# 	def setUp(self):
-# 		hopsapp.app.config['TESTING'] = True
-# 		self.app = hopsapp.app.test_client()
-# 		self.response = self.app.get("/")
-# 		self.soup = BeautifulSoup(self.response.data,'html.parser')
-# 	# testing if home is getting beer name from database and showing on page when page load
-# 	def test_home_beer_name(self):
-# 		response = self.app.get('/')
-# 		self.assertIn(b'HEADY TOPPER', response.data)
 
-# 	# testing if home is getting Brewery name from database and showing on page when page load
-# 	def test_home_brewery_name(self):
-# 		response = self.app.get('/')
-# 		self.assertIn(b'CARTON', response.data)
+class TestIntegration(unittest.TestCase):
+	def setUp(self):
+		hopsapp.app.config['TESTING'] = True
+		self.app = hopsapp.app.test_client()
+		self.response = self.app.get("/")
+		self.soup = BeautifulSoup(self.response.data,'html.parser')
+	# testing if home is getting beer name from database and showing on page when page load
+	def test_home_beer_name(self):
+		response = self.app.get('/')
+		self.assertIn(b'HEADY TOPPER', response.data)
 
-# 	# testing if home is getting beer style from database and showing on page when page load
-# 	def test_home_beer_style(self):
-# 		response = self.app.get('/')
-# 		self.assertIn(b'ALE', response.data)
+	# testing if home is getting Brewery name from database and showing on page when page load
+	def test_home_brewery_name(self):
+		response = self.app.get('/')
+		self.assertIn(b'CARTON', response.data)
 
-# 	# testing if home is getting beer ABV from database and showing on page when page load
-# 	def test_home_beer_ABV(self):
-# 		response = self.app.get('/')
-# 		self.assertIn(b'6.00%', response.data)
+	# testing if home is getting beer style from database and showing on page when page load
+	def test_home_beer_style(self):
+		response = self.app.get('/')
+		self.assertIn(b'ALE', response.data)
 
-# 	# testing if home is getting beer rating from database and showing on page when page load
-# 	def test_home_beer_rating(self):
-# 		response = self.app.get('/')
-# 		self.assertIn(b'0.0', response.data)
+	# testing if home is getting beer ABV from database and showing on page when page load
+	def test_home_beer_ABV(self):
+		response = self.app.get('/')
+		self.assertIn(b'6.00%', response.data)
 
-# 		# testing if home is getting beer rarity from database and showing on page when page load
-# 	def test_home_beer_rarity(self):
-# 		response = self.app.get('/')
-# 		self.assertIn(b'COMMON', response.data)
+	# testing if home is getting beer rating from database and showing on page when page load
+	def test_home_beer_rating(self):
+		response = self.app.get('/')
+		self.assertIn(b'0.0', response.data)
+
+		# testing if home is getting beer rarity from database and showing on page when page load
+	def test_home_beer_rarity(self):
+		response = self.app.get('/')
+		self.assertIn(b'COMMON', response.data)
 
 
 
